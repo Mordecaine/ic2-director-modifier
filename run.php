8c95990dd1e748d098a12783b3d983248e56203f
moreamazingnick <github@nicolas-schneider.at>
2022-02-14 10:25:31 +0100

    Update run.php

--- run.php
+++ run.php
@@ -2,3 +2,4 @@
 namespace Icinga\Module\Directorextension;
 use Icinga\Application\Modules\Module;
 $this->provideHook('director/PropertyModifier', Icinga\Module\Directorextension\ProvidedHook\Director\PropertyModifier\PropertyModifierRegexSearch);
+?>
